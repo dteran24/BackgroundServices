2588979313fa5b9be1cc7e1ebb31b129cedfb365
dteran24 <dteran21@yahoo.com>
2022-07-19 09:55:00 -0500

    Binding a service

--- app/src/main/java/com/example/backgroundservices/MainActivity.java
+++ app/src/main/java/com/example/backgroundservices/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.backgroundservices;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,31 @@ public class MainActivity extends AppCompatActivity {
     public void startService(View view) {
             Intent servIntent = new Intent(this, MyService.class);
             servIntent.putExtra("musicname","sample.mp3");
-            startService(servIntent);
+            //startService(servIntent); //This creates a service
+            bindService(servIntent, serviceConnection, BIND_AUTO_CREATE);
+
+
+    }
+
+    ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder mBinder) {
+            MyService.LocalBinder binder = (MyService.LocalBinder) mBinder;
+            MyService myService = binder.getService();
+            int randomNum = myService.getRandomNumber();
+            String getFile = myService.getWeatherJson("Houston");
+            Log.i(TAG, "Random num is "+randomNum);
+            Log.i(TAG, "Weather is "+ getFile);
+
         }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
+
+
+
 
     }
--- app/src/main/java/com/example/backgroundservices/MyService.kt
+++ app/src/main/java/com/example/backgroundservices/MyService.kt
@@ -3,12 +3,25 @@ package com.example.backgroundservices
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import java.util.*
 
 class MyService : Service() {
     companion object{
         var TAG = MyService::class.java.simpleName
+    }
+
+    private val mBinder = LocalBinder()
+
+    private val mGenerator = Random()
+
+    val randomNumber: Int
+        get() = mGenerator.nextInt(9999)
+
+    fun getWeatherJson(cityName:String):String{
+        return  "{temp:23}"+cityName
     }
 
     override fun onCreate() {
@@ -32,6 +45,12 @@ class MyService : Service() {
     }
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return mBinder
     }
+
+    inner class LocalBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        fun getService(): MyService = this@MyService
+    }
+
 }
